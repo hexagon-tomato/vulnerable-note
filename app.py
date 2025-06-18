a61a3a42e32c66c202097a57925d1dcd19abc5e3
hexagon-tomato <hexagon.tomato@gmail.com>
2025-06-18 20:32:33 +0900

    Add basic layout and index page

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+# render_template をインポート
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, Vulnerable Note!'
+def index(): # 関数名をhello_worldからindexに変更（分かりやすくするため）
+    # templatesフォルダのindex.htmlを探して表示する
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
